de1162a97128590f5874b2129c88e092127b8e2c
Wening Tyas Putri <dellarobbiaodd7@gmail.com>
2023-08-11 12:34:02 +0700

    update 1

--- src/main/java/com/juaracoding/Main.java
+++ src/main/java/com/juaracoding/Main.java
@@ -1,4 +1,43 @@
 package com.juaracoding;
 
+import com.juaracoding.drivers.DriverSingleton;
+import com.juaracoding.pages.DashboardPage;
+import com.juaracoding.pages.LoginPage;
+import com.juaracoding.utils.Scroll;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
 public class Main {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        DriverSingleton.getInstance("chrome");
+        driver = DriverSingleton.getDriver();
+        driver.get("https://shop.demoqa.com/");
+        System.out.println("Buka Browser dan buka URL");
+
+        loginTest(driver);
+
+        DriverSingleton.delay(3);
+        DriverSingleton.closeObjectInstance();
+    }
+
+    public static void loginTest(WebDriver driver){
+        Scroll scroll = new Scroll(driver);
+        scroll.scrollBy(0, 200);
+        System.out.println("Scroll ke bawah, utk munculkan header");
+
+        DashboardPage dashboard = new DashboardPage();
+        dashboard.menuClick("Login");
+        System.out.println("Klik menu My Account");
+
+        scroll.scrollBy(0, 500);
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+        System.out.println("Test Login");
+
+        scroll.scrollBy(0, 500);
+        loginPage.verifikasi();
+    }
 }
--- src/main/java/com/juaracoding/drivers/strategies/Firefox.java
+++ src/main/java/com/juaracoding/drivers/strategies/Firefox.java
@@ -1,4 +1,14 @@
 package com.juaracoding.drivers.strategies;
 
-public class Firefox {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firefox implements DriverStrategy{
+    @Override
+    public WebDriver setStrategy() {
+        String path = "E:\\Bootcam\\App\\GeckoDriver\\geckodriver.exe";
+        System.setProperty("webdriver.chrome.driver", path);
+        WebDriver driver = new FirefoxDriver();
+        return driver;
+    }
 }
