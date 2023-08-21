d550f8d6a4f6b6c8b5fb3b46bdeab7336c8d1819
Wening Tyas Putri <46559795+WeningTyas@users.noreply.github.com>
2023-08-21 20:25:00 +0700

    Update ChartPage.java
    update error

--- src/main/java/com/juaracoding/pages/ChartPage.java
+++ src/main/java/com/juaracoding/pages/ChartPage.java
@@ -1,4 +1,24 @@
 package com.juaracoding.pages;
 
+import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ChartPage {
+    private WebDriver driver;
+
+    public ChartPage(){
+        this.driver = DriverSingleton.getDriver();
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//a[@class='button wc-backward']")
+    WebElement btnReturnShop;
+
+    public void btnReturnShopClick(){
+        btnReturnShop.click();
+    }
 }
+
